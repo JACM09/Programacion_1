7886382845c4a1e607c8030b8afbb10f88c4bb30
Joseph Castro <Joseph Castro@DESKTOP-B9HHQB5>
2026-08-11 05:15:33 -0600

    Clase 24/7/26

--- CalculadoraBasica/src/calculadorabasica/CalculadoraBasica.java
+++ CalculadoraBasica/src/calculadorabasica/CalculadoraBasica.java
@@ -34,10 +34,8 @@ public class CalculadoraBasica {
         int num1 = 0;
         int num2 = 0;
         char res = 'a';
-        int total = 0;
-        double totalD = 0;
+        double total = 0;
         double porce = 0;
-        double totalP = 0;
         
         System.out.println("Bienvenido a su Calculadora Basica");
         System.out.println("Por favor ingrese su primer numero");
@@ -47,25 +45,25 @@ public class CalculadoraBasica {
         num2 = entrada.nextInt();
         
         System.out.println("Operaciones a realizar elija una");
-        System.out.println("S = SUMA || R = RESTA || D = DIVISION || M = MULTIPLICACION || P = PORCENTAJE ");
+        System.out.println("S = SUMA || R = RESTA || D = DIVISION || M = MULTIPLICACION || P = PORCENTAJE || % = MODULO ");
         entrada.nextLine();
         res = entrada.next().toUpperCase().charAt(0);
         
         switch (res){
             case 'S':
                 total= num1 + num2;
-                System.out.printf("La suma de %d + %d es: %d",num1,num2,total);
+                System.out.printf("La suma de %d + %d es: %.2f",num1,num2,total);
                 break;
             case 'R':
                 total= num1 - num2;
-                System.out.printf("La resta de %d - %d es: %d",num1,num2,total);
+                System.out.printf("La resta de %d - %d es: %.2f",num1,num2,total);
                 break; 
             case 'D':
                 
                 if(num2!=0){
                 
-                    totalD = num1 / (double)num2;
-                    System.out.printf("La division de %d / %d es: %.2f",num1,num2,totalD);
+                    total = num1 / (double)num2;
+                    System.out.printf("La division de %d / %d es: %.2f",num1,num2,total);
                 
                 } else {
                 
@@ -75,18 +73,25 @@ public class CalculadoraBasica {
                 break;
             case 'M':
                 total= num1 * num2;
-                System.out.printf("La multiplicacion de %d * %d es: %d",num1,num2,total);
-                break; 
+                System.out.printf("La multiplicacion de %d * %d es: %.2f",num1,num2,total);
+                break;
+            case '%':
+                if (num2 != 0) {
+                    total = num1 % num2;
+                    System.out.printf("El modulo (residuo) de %d %% %d es: %.2f",num1,num2,total);
+                } else {
+                    System.out.println("ERROOOR NO SE PUEDE SACAR MODULO ENTRE CERO");
+                }
+                break;    
             case 'P':
                 porce = (double)num2 / 100;
-                totalP = num1 * porce;
-                System.out.printf("El primer numero que ingreso es %d el porcentaje que desea sacar es %dxciento el total es %.2f",num1,num2,totalP);
-                
+                total = num1 * porce;
+                System.out.printf("El primer numero que ingreso es %d el porcentaje que desea sacar es %d%% el total es %.2f%%",num1,num2,total);              
                 break; 
             default:
                 System.out.println("Opcion ingresada no valida");
         }
         
-    }
+    } //Fin de Main
     
-}
+} //Fin de Class
